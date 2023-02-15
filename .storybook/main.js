f32393dc943d34f0c4a2d9d2dc8c486ee11213b1
takayuki nakahashi <takayuki.nakahashi@todoker.com>
2023-02-16 00:52:25 +0900

    renew storybook and upgrade react, add framer-motion

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,20 @@
-module.exports = {
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
   "stories": [
-    "../**/*.stories.mdx",
-    "../**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react"
-}
+  "framework": {
+    "name": "@storybook/react-webpack5",
+    "options": {}
+  },
+  "docs": {
+    "autodocs": "tag"
+  }
+};
+export default config;
