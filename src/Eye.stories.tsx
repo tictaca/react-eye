fed6ec9fc756f93b96059a5c47eb6a86b3f16975
takayuki nakahashi <takayuki.nakahashi@todoker.com>
2023-02-15 22:47:22 +0900

    accommodate to ellipse shape

--- src/Eye.stories.tsx
+++ src/Eye.stories.tsx
@@ -50,3 +50,77 @@ export const Primary = () => {
     </div>
   );
 };
+
+export const DifferentRatio = () => {
+  return (
+    <div
+      style={{
+        position: "relative",
+        width: "100%",
+        height: "100vh",
+        backgroundColor: "#DDD",
+      }}
+    >
+      <Eye
+        width={100}
+        height={200}
+        irisWidth={60}
+        irisHeight={80}
+        irisColor={"#000"}
+        style={{
+          position: "absolute",
+          top: 100,
+          left: 100
+        }}
+      />
+      <Eye
+        width={200}
+        height={100}
+        irisWidth={80}
+        irisHeight={60}
+        irisColor={"#000"}
+        style={{
+          position: "absolute",
+          top: 100,
+          left: 300
+        }}
+      />
+      <Eye
+        width={100}
+        height={100}
+        irisWidth={80}
+        irisHeight={80}
+        irisColor={"#000"}
+        style={{
+          position: "absolute",
+          top: 100,
+          left: 550
+        }}
+      />
+      <Eye
+        width={300}
+        height={200}
+        irisWidth={10}
+        irisHeight={80}
+        irisColor={"#000"}
+        style={{
+          position: "absolute",
+          top: 100,
+          left: 800
+        }}
+      />
+      <Eye
+        width={100}
+        height={100}
+        irisWidth={30}
+        irisHeight={80}
+        irisColor={"#000"}
+        style={{
+          position: "absolute",
+          top: 400,
+          left: 800
+        }}
+      />
+    </div>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
+import { v4 as uuidV4 } from "uuid";
 
 type Props = {
   width: number;
@@ -11,11 +12,13 @@ type Props = {
 };
 
 const Eye: React.FC<Props> = (props) => {
-  const { width, height, irisWidth, irisHeight, irisColor, className, style } = props;
+  const { width, height, irisWidth, irisHeight, irisColor, className, style } =
+    props;
   const [targetPosition, setTargetPosition] = useState({ x: 0, y: 0 });
   const [irisPosition, setIrisPosition] = useState({ x: 0, y: 0 });
   const eyeRef = useRef(null);
   const irisRef = useRef(null);
+  const clipId = uuidV4();
   useEffect(() => {
     const reactMouse = (e) => {
       setTargetPosition({ x: e.pageX, y: e.pageY });
@@ -27,26 +30,39 @@ const Eye: React.FC<Props> = (props) => {
     if (eyeRef.current && irisRef.current) {
       const currentRect = eyeRef.current.getBoundingClientRect();
       const currentIrisRect = irisRef.current.getBoundingClientRect();
-      const distanceX =
-        targetPosition.x - (currentRect.left + width / 2 + window.scrollX);
-      const distanceY =
-        targetPosition.y - (currentRect.top + height / 2 + window.scrollY);
-      const r = currentRect.width / 2 - currentIrisRect.width / 2;
-      const deg = Math.atan(distanceY / distanceX);
-      if (distanceX * distanceX + distanceY * distanceY < r * r) {
+      const distance = {
+        x: targetPosition.x - (currentRect.left + width / 2 + window.scrollX),
+        y: targetPosition.y - (currentRect.top + height / 2 + window.scrollY),
+      };
+      const insideRangeEllipse = {
+        x: currentRect.width / 2 - currentIrisRect.width / 2,
+        y: currentRect.height / 2 - currentIrisRect.height / 2,
+      };
+      const deg = Math.atan2(distance.y, distance.x);
+      if (
+        //inside of ellipse: ｘ^2／ａ^2＋ｙ^2／ｂ^2＝１
+        distance.x ** 2 / insideRangeEllipse.x ** 2 +
+          distance.y ** 2 / insideRangeEllipse.y ** 2 <=
+        1
+      ) {
         setIrisPosition({
           x: targetPosition.x - currentRect.x,
           y: targetPosition.y - currentRect.y,
         });
-      } else if (distanceX >= 0) {
-        setIrisPosition({
-          x: Math.cos(deg) * r + currentRect.width / 2,
-          y: Math.sin(deg) * r + currentRect.height / 2,
-        });
       } else {
+        const degToCalc = deg;
+        //x=1/√((1/a)^2+(tanθ/b)^2)
+        const x =
+          1 /
+          Math.sqrt(
+            (1 / insideRangeEllipse.x) ** 2 +
+              (Math.tan(degToCalc) / insideRangeEllipse.y) ** 2
+          );
         setIrisPosition({
-          x: Math.cos(deg + (180 * Math.PI) / 180) * r + currentRect.width / 2,
-          y: Math.sin(deg + (180 * Math.PI) / 180) * r + currentRect.height / 2,
+          x: x * (distance.x < 0 ? -1 : 1) + currentRect.width / 2,
+          y:
+            x * Math.tan(degToCalc) * (distance.x < 0 ? -1 : 1) +
+            currentRect.height / 2,
         });
       }
     }
@@ -59,20 +75,34 @@ const Eye: React.FC<Props> = (props) => {
       className={className}
       style={style}
     >
-      <circle
+      <defs>
+        <clipPath id={clipId}>
+          <ellipse
+            cx={width / 2}
+            cy={height / 2}
+            rx={width / 2}
+            ry={height / 2}
+          />
+        </clipPath>
+      </defs>
+      <g clipPath={`url(#${clipId})`}>
+      <ellipse
         ref={eyeRef}
         cx={width / 2}
         cy={height / 2}
-        r={width / 2}
+        rx={width / 2}
+        ry={height / 2}
         fill="#FFF"
       />
-      <circle
+      <ellipse
         ref={irisRef}
         cx={irisPosition.x}
         cy={irisPosition.y}
-        r={irisWidth / 2}
+        rx={irisWidth / 2}
+        ry={irisHeight / 2}
         fill={irisColor}
       />
+      </g>
     </svg>
   );
 };
